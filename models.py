e0195128586e7ecb9af1d7a30cda6b371a13a6ee
npeard <nolan.peard@gmail.com>
2024-09-25 10:23:19 -0700

    fixed reshaping in fully-connected layers

--- models.py
+++ models.py
@@ -12,33 +12,42 @@ class CNN(nn.Module):
         self.in_channels = in_channels
         print("self.in_channels = ", self.in_channels)
         self.conv_layers = nn.Sequential(
-            nn.Conv1d(in_channels, 16, kernel_size=7),  # Lout = 250, given L = 256
+            nn.Conv1d(in_channels, 16, kernel_size=7),
+            # Lout = 250, given L = 256
             act_fn_by_name[activation],
-            nn.MaxPool1d(2),  # Lout = 125, given L = 250
-            nn.Conv1d(16, 32, kernel_size=7),  # Lout = 119, given L = 125
+            nn.MaxPool1d(2),
+            # Lout = 125, given L = 250
+            nn.Conv1d(16, 32, kernel_size=7),
+            # Lout = 119, given L = 125
             act_fn_by_name[activation],
-            nn.MaxPool1d(2),  # Lout = 59, given L = 119
-            nn.Conv1d(32, 64, kernel_size=7),  # Lout = 53, given L = 59
+            nn.MaxPool1d(2),
+            # Lout = 59, given L = 119
+            nn.Conv1d(32, 64, kernel_size=7),
+            # Lout = 53, given L = 59
             act_fn_by_name[activation],
-            nn.MaxPool1d(2),  # Lout = 26, given L = 53
+            nn.MaxPool1d(2),
+            # Lout = 26, given L = 53
             nn.Dropout(0.1),
-            nn.Conv1d(64, 64, kernel_size=7),  # Lout = 20, given L = 26
+            nn.Conv1d(64, 64, kernel_size=7),
+            # Lout = 20, given L = 26
             act_fn_by_name[activation],
-            nn.MaxPool1d(2)  # Lout = 10, given L = 20
+            nn.MaxPool1d(2)
+            # Lout = 10, given L = 20
         )
         self.fc_layers = nn.Sequential(
-            #nn.Linear(640, 16),
-            nn.Linear(320, 16),
+            # length of input = 64 filters * length of 10 left
+            nn.Linear(640, 16),
             nn.ReLU(),
             nn.Linear(16, output_size)
         )
 
     def forward(self, x):
-        print("x.shape", x.shape)
+        # print("x.shape", x.shape)
         out = self.conv_layers(x)
         # print(f"post conv out size: {out.size()}")  # [128, 64, 10]
-        out = out.view(out.size(0), self.in_channels, -1)
-        print(f"post conv out reshaped size: {out.size()}")  # confirmed [128, 1, 640]
+        out = out.view(out.size(0), 1, -1)
+        # print(f"post conv out reshaped size: {out.size()}")  # confirmed [
+        # 128, 1, 640]
         out = self.fc_layers(out)  # expect out [128, 1, 1]
         # print(f"post fc out size: {out.size()}") # confirmed: [128, 1, 1]
         return out
