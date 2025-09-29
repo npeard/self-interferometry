9e23cd4ab6a441092996f0254c016efea5e58ecb
npeard <nolan.peard@gmail.com>
2025-09-29 10:04:10 -0700

    minor project linter updates

--- self_interferometry/acquisition/redpitaya/__init__.py
+++ self_interferometry/acquisition/redpitaya/__init__.py
@@ -4,4 +4,4 @@ from .manager import RedPitayaManager
 from .redpitaya_config import RedPitayaConfig
 from .scpi import scpi
 
-__all__ = ['RedPitayaManager', 'RedPitayaConfig', 'scpi']
+__all__ = ['RedPitayaConfig', 'RedPitayaManager', 'scpi']
