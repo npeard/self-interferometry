654f1ff4af133899c182a03f3fa8e86b8bb9bae6
npeard <nolan.peard@gmail.com>
2025-10-13 11:19:51 -0700

    updated calib parameters

--- self_interferometry/acquisition/simulations/calib_params.py
+++ self_interferometry/acquisition/simulations/calib_params.py
@@ -1,6 +1,25 @@
 from dataclasses import dataclass
 
 
+# @dataclass
+# class CalibrationParameters:
+#     """Calibration parameters for the coil driver.
+
+#     Attributes:
+#         f0: Resonant frequency in Hz
+#         Q: Quality factor
+#         k: Gain factor
+#         c: Phase offset
+#         speaker_part_number: Optional part number of the speaker
+#     """
+
+#     f0: float = 257.20857316296724
+#     Q: float = 15.804110908084784
+#     k: float = 33.42493417407945
+#     c: float = -3.208233068626455
+#     speaker_part_number: str | None = None
+
+
 @dataclass
 class CalibrationParameters:
     """Calibration parameters for the coil driver.
@@ -13,8 +32,8 @@ class CalibrationParameters:
         speaker_part_number: Optional part number of the speaker
     """
 
-    f0: float = 257.20857316296724
-    Q: float = 15.804110908084784
-    k: float = 33.42493417407945
-    c: float = -3.208233068626455
+    f0: float = 258.936
+    Q: float = 16.98
+    k: float = 14.553
+    c: float = -3.220
     speaker_part_number: str | None = None
